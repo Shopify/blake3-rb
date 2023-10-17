ae39e39147a5b0ba41489a7c8f5e3647ecf37867
Brad Dietrich <brad.dietrich@shopify.com>
2023-10-17 12:55:34 -0400

    Define class in ruby so typing can find it more easily

--- lib/digest/blake3.rb
+++ lib/digest/blake3.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require "digest"
+
+module Digest
+  class Blake3 < Digest::Base
+  end
+end
+
 # Tries to require the precompiled extension for the given Ruby version first
 begin
   RUBY_VERSION =~ /(\d+\.\d+)/
