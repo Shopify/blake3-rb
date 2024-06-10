7878a32f62c74ebd5809696ac19ae8483571f31f
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2024-06-10 14:39:14 -0400

    Support dynamic symbol resolution in ruby 3.4+

--- ext/digest/blake3_ext/src/bindings.rs
+++ ext/digest/blake3_ext/src/bindings.rs
@@ -1,6 +1,5 @@
+use rb_sys::{size_t, VALUE};
 use std::ffi::{c_int, c_uchar, c_void};
-
-use rb_sys::{rb_data_object_wrap, rb_obj_freeze, size_t, VALUE};
 
 pub const RUBY_DIGEST_API_VERSION: c_int = 3;
 
@@ -20,7 +19,40 @@ pub struct RbDigestMetadataT {
     pub finish_func: RbDigestHashFinishFuncT,
 }
 
+#[cfg(ruby_gt_3_3)]
 pub unsafe fn rb_digest_make_metadata(meta: &'static RbDigestMetadataT) -> VALUE {
+    static mut WRAPPER: Option<unsafe extern "C" fn(&'static RbDigestMetadataT) -> VALUE> = None;
+
+    unsafe fn load_wrapper() {
+        use rb_sys::rb_ext_resolve_symbol;
+        use std::ffi::c_char;
+        use std::sync::Once;
+
+        static INIT: Once = Once::new();
+
+        INIT.call_once(|| {
+            let lib_name = "digest.so\0".as_ptr() as *const c_char;
+            let symbol_name = "rb_digest_wrap_metadata\0".as_ptr() as *const c_char;
+            let symbol_ptr = rb_ext_resolve_symbol(lib_name, symbol_name);
+
+            if !symbol_ptr.is_null() {
+                WRAPPER = Some(std::mem::transmute(symbol_ptr));
+            } else {
+                panic!("Failed to resolve rb_digest_wrap_metadata");
+            }
+        });
+    }
+
+    load_wrapper();
+    if let Some(wrapper) = WRAPPER {
+        return wrapper(meta);
+    }
+    panic!("Failed to resolve rb_digest_wrap_metadata");
+}
+
+#[cfg(not(ruby_gt_3_3))]
+pub unsafe fn rb_digest_make_metadata(meta: &'static RbDigestMetadataT) -> VALUE {
+    use rb_sys::{rb_data_object_wrap, rb_obj_freeze};
     let data = rb_data_object_wrap(
         0 as VALUE,
         meta as *const RbDigestMetadataT as *mut c_void,
