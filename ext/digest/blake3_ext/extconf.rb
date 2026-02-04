60a370b42b0b157ecfd1f43eafae37a83472f09f
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2026-02-04 17:03:30 -0500

    Use runtime SIMD detection instead of build-time
    BLAKE3's blake3_dispatch.c already implements runtime CPU feature
    detection using cpuid. This change ensures all x86 SIMD implementations
    (SSE2, SSE4.1, AVX2, AVX512) are always compiled on x86 platforms,
    allowing the runtime dispatcher to select the optimal implementation
    based on the actual CPU features available.
    
    Changes:
    - Remove --disable-* flags for individual SIMD extensions
    - Always compile all x86 SIMD implementations on x86 platforms
    - Simplify ARM32 NEON detection using compiler macros
    - Update comments to clarify runtime detection behavior

--- ext/digest/blake3_ext/extconf.rb
+++ ext/digest/blake3_ext/extconf.rb
@@ -9,12 +9,10 @@ extension_name = "blake3_ext"
 blake3_dir = File.join(__dir__, "blake3")
 
 # Detect architecture and platform
-is_x86_64 = RbConfig::CONFIG["host_cpu"] =~ /x86_64|amd64/i
-is_x86_32 = RbConfig::CONFIG["host_cpu"] =~ /i[3-6]86/i
+is_x86 = RbConfig::CONFIG["host_cpu"] =~ /x86_64|amd64|i[3-6]86/i
 is_arm64 = RbConfig::CONFIG["host_cpu"] =~ /aarch64|arm64/i
 is_arm32 = RbConfig::CONFIG["host_cpu"] =~ /arm/i && !is_arm64
 is_windows = RbConfig::CONFIG["host_os"] =~ /mswin|mingw|cygwin/i
-RbConfig::CONFIG["host_os"] =~ /darwin/i
 
 # Add BLAKE3 source directory to include path
 $INCFLAGS << " -I#{blake3_dir}"
@@ -27,43 +25,21 @@ srcs = [
 ]
 
 # Add platform-specific SIMD implementations
-if is_x86_64 || is_x86_32
-  # x86/x86_64: Use intrinsics-based implementations
-  # We compile each SIMD file separately with appropriate flags
-
-  # SSE2 support (baseline for x86_64)
-  if arg_config("--disable-sse2")
-    $CFLAGS << " -DBLAKE3_NO_SSE2"
-  else
-    srcs << "blake3/blake3_sse2.c"
-  end
-
-  # SSE4.1 support
-  if arg_config("--disable-sse41")
-    $CFLAGS << " -DBLAKE3_NO_SSE41"
-  else
-    srcs << "blake3/blake3_sse41.c"
-  end
-
-  # AVX2 support
-  if arg_config("--disable-avx2")
-    $CFLAGS << " -DBLAKE3_NO_AVX2"
-  else
-    srcs << "blake3/blake3_avx2.c"
-  end
-
-  # AVX512 support
-  if arg_config("--disable-avx512")
-    $CFLAGS << " -DBLAKE3_NO_AVX512"
-  else
-    srcs << "blake3/blake3_avx512.c"
-  end
+# BLAKE3's blake3_dispatch.c handles runtime CPU feature detection automatically
+if is_x86
+  # x86/x86_64: Include ALL SIMD implementations
+  # Runtime detection in blake3_dispatch.c will choose the best one
+  srcs << "blake3/blake3_sse2.c"
+  srcs << "blake3/blake3_sse41.c"
+  srcs << "blake3/blake3_avx2.c"
+  srcs << "blake3/blake3_avx512.c"
 elsif is_arm64
-  # AArch64: NEON is enabled by default
+  # AArch64: NEON is always available and enabled by default
   srcs << "blake3/blake3_neon.c"
 elsif is_arm32
-  # ARM32: NEON support must be explicitly enabled
-  if arg_config("--enable-neon")
+  # ARM32: Check for NEON support at compile time
+  # NEON is optional on 32-bit ARM
+  if have_macro("__ARM_NEON") || have_macro("__ARM_NEON__")
     srcs << "blake3/blake3_neon.c"
     $CFLAGS << " -DBLAKE3_USE_NEON=1"
   end
@@ -79,10 +55,8 @@ $CFLAGS << " -O3"
 # Enable position-independent code for shared library
 $CFLAGS << " -fPIC" unless is_windows
 
-# On x86/x86_64, we need to set compiler flags for each SIMD implementation
-# This is handled via separate rules in the Makefile
-if is_x86_64 || is_x86_32
-  # Create custom compilation rules for SIMD files
+# On x86, we need to compile each SIMD file with appropriate flags
+if is_x86
   $objs = $srcs.map { |f| File.basename(f, ".*") + ".o" }
 end
 
@@ -91,12 +65,16 @@ dir_config(extension_name)
 create_makefile("digest/blake3/#{extension_name}")
 
 # Append custom rules for SIMD compilation on x86
-if is_x86_64 || is_x86_32
+# Each SIMD implementation requires specific compiler flags to enable
+# the corresponding instruction set, but runtime detection chooses which to use
+if is_x86
   makefile = File.read("Makefile")
 
   simd_rules = <<~RULES
 
     # SIMD-specific compilation rules
+    # Each file is compiled with flags to enable that instruction set
+    # Runtime detection in blake3_dispatch.c selects the best implementation
     blake3_sse2.o: $(srcdir)/blake3/blake3_sse2.c
     \t$(ECHO) compiling $(<)
     \t$(Q) $(CC) $(INCFLAGS) $(CPPFLAGS) $(CFLAGS) -msse2 -c -o $@ $<
