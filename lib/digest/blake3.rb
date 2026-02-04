8480d2ea98a6d2d0bc74987b2369729a9bb44740
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2026-02-04 16:59:20 -0500

    Simplify extension loading in Ruby wrapper
    - Remove version-specific precompiled binary detection
    - C extension is compiled from source during gem install

--- lib/digest/blake3.rb
+++ lib/digest/blake3.rb
@@ -7,10 +7,4 @@ module Digest
   end
 end
 
-# Tries to require the precompiled extension for the given Ruby version first
-begin
-  RUBY_VERSION =~ /(\d+\.\d+)/
-  require "digest/blake3/#{Regexp.last_match(1)}/blake3_ext"
-rescue LoadError
-  require_relative "blake3/blake3_ext"
-end
+require_relative "blake3/blake3_ext"
